003855776162a770953a1d1de7e93fc525457d40
Zual Rana <freelancerzual@gmail.com>
2025-01-10 22:09:16 +0600

    successfully load all post

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const loadAllPost = async () => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/retro-forum/posts`
+  );
+  const data = await res.json();
+  displayData(data.posts);
+};
+
+const displayData = (posts) => {
+  console.log(posts);
+};
+
+loadAllPost();
